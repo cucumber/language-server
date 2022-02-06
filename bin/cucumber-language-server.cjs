422c6d559f4d6dec9deeb4609bb4659a879aa63e
William Boman <william@redwill.se>
2022-02-06 13:15:11 +0100

    bin: fix startServer import

--- bin/cucumber-language-server.cjs
+++ bin/cucumber-language-server.cjs
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 require('source-map-support').install()
-const startServer = require('../dist/cjs/src/startServer')
+const { startServer } = require('../dist/cjs/src/startServer')
 
 const wasmUrls = {
   java: `node_modules/@cucumber/language-service/tree-sitter-java.wasm`,
