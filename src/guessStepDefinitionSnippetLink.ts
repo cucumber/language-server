9994b15d3a17ae38572b26b6664cd5fa20587187
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2022-05-24 15:01:28 +0100

    Clean up

--- src/guessStepDefinitionSnippetLink.ts
+++ src/guessStepDefinitionSnippetLink.ts
@@ -21,51 +21,7 @@ export async function guessStepDefinitionSnippetLink(
       targetSelectionRange: targetRange,
     }
   } else {
+    // TODO: try to guess extension and path by looking for package.json, pom.xml etc
     return undefined
   }
-
-  // const paths = await findPaths(glueGlobs)
-  // if (paths.length > 0) {
-  //   return resolve(paths[0])
-  // } else if (glueGlobs.length > 0) {
-  //   const glob0 = glueGlobs[0]
-  //   const tasks = fg.generateTasks(glob0)
-  //   const globPath = tasks[0].positive[0].replace('**/', '')
-  //   let globExt = extname(globPath)
-  //   if (globExt === '') {
-  //     globExt = await guessExtension()
-  //   }
-  //   const dir = dirname(globPath)
-  //   const path = join(dir, `stepdefs${globExt}`)
-  //   return resolve(path)
-  // } else {
-  //   throw new Error(
-  //     'The glue globs are empty. Cannot determine where to write the step definition snippet'
-  //   )
-  // }
 }
-
-// const fileAssociations = {
-//   '.ts': ['tsconfig.json'],
-//   '.js': ['package.json'],
-//   '.java': ['pom.xml', '*.gradle'],
-//   '.php': ['composer.json'],
-//   '.cs': ['*.sln'],
-//   '.rb': ['Rakefile', 'Gemfile', '*.gemspec'],
-// }
-//
-// async function guessExtension(): Promise<string> {
-//   let allGlobs: string[] = []
-//   for (const [ext, globs] of Object.entries(fileAssociations)) {
-//     allGlobs = allGlobs.concat(...globs)
-//     const files = await findPaths(globs)
-//     if (files.length > 0) return ext
-//   }
-//   throw new Error(
-//     `Unable to determine extension for new step definition. Couldn't find any files matching these globs: ${JSON.stringify(
-//       allGlobs,
-//       null,
-//       2
-//     )}`
-//   )
-// }
