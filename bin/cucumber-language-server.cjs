afd3798419c69ee723b561c4e3136af802f05179
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2022-07-14 15:32:17 +0100

    Use wasm for standalone mode. Fixes #56

--- bin/cucumber-language-server.cjs
+++ bin/cucumber-language-server.cjs
@@ -1,4 +1,5 @@
 #!/usr/bin/env node
+/* eslint-disable @typescript-eslint/no-var-requires */
 require('source-map-support').install()
-const { startNodeServer } = require('../dist/cjs/src/node/startNodeServer')
-startNodeServer()
+const { startWasmServer } = require('../dist/cjs/src/wasm/startWasmServer')
+startWasmServer(`${__dirname}/../node_modules/@cucumber/language-service/dist`)
